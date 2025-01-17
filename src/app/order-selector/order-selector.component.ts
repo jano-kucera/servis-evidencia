f53588c61a30ccc47f838d476d88a9d5762b6b4c
jano-kucera <janislavkucera@gmail.com>
2025-01-17 22:24:04 +0100

    Simple cards from orders

--- src/app/order-selector/order-selector.component.ts
+++ src/app/order-selector/order-selector.component.ts
@@ -1,6 +1,8 @@
 import { JsonPipe } from "@angular/common";
 import { Component, inject } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
 import type { Order } from "../models/order";
 import { OrdersService } from "./orders.service";
 
@@ -8,8 +10,14 @@ import { OrdersService } from "./orders.service";
  * Order selector component.
  */
 @Component({
-    imports: [FormsModule, JsonPipe],
+    imports: [
+        FormsModule,
+        JsonPipe,
+        MatButtonModule,
+        MatCardModule,
+    ],
     selector: "app-order-selector",
+    styleUrl: "./order-selector.component.scss",
     templateUrl: "./order-selector.component.html",
 })
 export class OrderSelectorComponent {
